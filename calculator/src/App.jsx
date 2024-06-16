ceaa9d343db9fb16fd79cf87d9745f6c6ab3a1f8
sudhanshu423 <sudhanshusi420@gmail.com>
2024-06-16 23:14:58 +0530

    Add files via upload

---
+++ calculator/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Calculator from './component/calculator';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+};
+
+export default App
